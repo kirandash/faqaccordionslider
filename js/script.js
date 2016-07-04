66aceb0e4f67f825a35cd3501defc6a708fe0635
kirandash <thekirankumardash@gmail.com>
2016-07-04 22:25:44 +0530

    020 FAQ Slider JavaScript

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
 // JavaScript Document
+//Accordion
+var action = "click";
+var speed = "500";
+
+$(document).ready(function(e) {
+   //Question handler
+    $('li.q').on(action, function(){
+		//Get next element
+		$(this).next()
+				.slideToggle(speed)
+					.siblings('li.a')
+						.slideUp();
+						
+		//Get image for active question
+		var img = $(this).children('img');				
+		//Remove the 'rotate' class for all images except the active
+		$('img').not(img).removeClass('rotate');
+		//Toggle rotate class
+		img.toggleClass('rotate');
+	});
+});
